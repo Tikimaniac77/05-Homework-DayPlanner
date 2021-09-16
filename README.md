82ce0923ad835f097849a7c64410d3ea3ae9d144
Tiki <michaelwachtel@yahoo.com>
2021-09-16 00:29:27 -0700

    added AMPM suffix to hours text. added js functions to look at current time using moment.js and compare to id of text area to change background color dynamically. added click function to listen for save button. added functions to store locale storage of user input to timeslots, and to save user input on page refresh. updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# 05-Homework-DayPlanner
+# 05-Homework
+
+## Description
+
+This is my Scheduler Homework for week 5 of Bootcamp
+
+Includes my Javascript, for the HTML and CSS sheets provided.
+
+The Quiz should meet the following criteria:
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Installation
+
+Download the repository when ready
+
+## Usage
+
+![screencap of the site is:](./Scheduler.png)
+
+## Credits
+
+[the url for the site is:](https://tikimaniac77.github.io/05-Homework-Scheduler/)
+
+## License
+
+MIT License
+
+Copyright (c) [2021] [Michael Thomas Wachtel]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this scodoftware and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,9 @@
 var hourSlot = 9;
+var hourAMPM = "AM";
+var idHourSlot = 9;
 
-/*function currentTime() {
-    var time = moment().format("LT");
-
-    //time = time.split('');
-
-    //var amPm = '';
-
-    //var hour = parseHour(time);
-
-    console.log(time);
-
-    //if(time[time.length - 2] === "past"])
-}*/
+var storedInput = [];
+var storedInputName = "Stored Input";
 
 
 // generates hour blocks/text using moment.js & generates text box for input, and save button/block. using jQuery.
@@ -26,17 +17,18 @@ function createHourSlots(events)
 
     for(var i = 0; i < events; i++) {
 
-        var textTime = hourSlot;
+        var textTime = hourSlot + hourAMPM;
+        var divID = idHourSlot; // need to adjust this to match same hours as each row time
         
         EcreateBlock = $('<div>').addClass('row py-1');
 
-        EcreateHourText = $('<h2>').addClass('text-center').text(textTime);
-        EcreateHourBlock = $('<div>').addClass('col-2 py-3 align-middle').append(EcreateHourText);
+        EcreateHourText = $('<h2>').addClass(' text-center').text(textTime);
+        EcreateHourBlock = $('<div>').addClass('hour col-2 py-3 align-middle border border-primary').append(EcreateHourText);
 
-        EcreateTextBlock = $('<textarea>').addClass('col-8 py-3 overflow-auto').text('').attr("id", textTime);
-        //slotColorBG($EcreateHourBlock, timeNow, textTime);
+        EcreateTextBlock = $('<textarea>').addClass('col-8 py-3 overflow-auto border border-primary').text('').attr("id", divID);
+        hourColors(EcreateTextBlock, timeNow, textTime);
 
-        EcreateSaveBtn = $('<button>').addClass('saveBtn').html('Save');
+        EcreateSaveBtn = $('<button>').addClass('saveBtn i:hover').html('Save');
         
         EcreateSaveBlock = $('<div>').addClass('col-1 py-3 saveBlock border border-primary').append(EcreateSaveBtn);
 
@@ -45,20 +37,107 @@ function createHourSlots(events)
         $('#scheduler').append(EcreateBlock);
 
         changeHourSlot();
+        changeIdHourSlot();
+        
     }
 }
+
+
+
+// note: missing proper time block classes that are MILITARY time - AA
+function hourColors() {
+    // get current hours using moment military time
+    var currentHour = moment().hours();
+    // loop through the timeblocks so we can check at each block
+    $('.row').each(function() {
+        var eachSlot = parseInt($(this).attr('id'))
+        console.log(eachSlot);
+        if( eachSlot < currentHour){
+            
+            $(this).addClass('past')
+        } else if ( eachSlot === currentHour){
+            
+            $(this).removeClass('past');
+            $(this).addClass('present')
+
+        } else {
+            
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    })
+    
+}
+
+
+
 
 //cycles hours displayed to page
 function changeHourSlot() {
     if (hourSlot === 12) {
+        //change hour to 1 after 12pm
         hourSlot = 1;
     } else if(hourSlot === 11) {
-        //need to add suffix for AM & PM
+        //to change AM to PM
+        hourAMPM = "PM";
         hourSlot++;
     } else {
         hourSlot++;
     }    
 }
+
+function changeIdHourSlot() {
+    if (idHourSlot === 12) {
+        //change hour to 1 after 12pm
+        idHourSlot = 1;
+    } else {
+        idHourSlot++;
+    }    
+}
+
+
+function alterStoredInput(Text,ID){
+    input = {
+        id :  ID,
+        input : Text.trim()
+    }
+
+    for(var i = 0; i < storedInput.length; i++){
+        
+        if(storedInput[i].id === input.id){
+            storedInput.splice(i, 1);
+
+            localStorage.setItem(storedInputName, JSON.stringify(storedInput));
+
+            return null;
+        }
+    }
+
+    storedInput.push(input);
+
+    localStorage.setItem(storedInputName, JSON.stringify(storedInput));
+}
+
+function getStoredInput(){
+
+    if(localStorage.getItem(storedInputName)){
+        storedInput = JSON.parse(localStorage.getItem(storedInputName));
+
+        storedInput.forEach(block => {
+
+            bID = "#" + block.id;
+
+            getBlock = $(document.getElementById(block.id));
+
+            getBlock.val(block.input);
+
+            save = $((getBlock).parent().children().children()[1])
+
+        });
+    }
+}
+
 
 // function displays current date and time using moment.js and adds it to currentDay id.
 function DisplayDate(Format)
@@ -71,3 +150,17 @@ function DisplayDate(Format)
 DisplayDate("LLLL");
 // set to 9 to limit amount of times function runs.
 createHourSlots(9);
+getStoredInput();
+
+$(".saveBtn").click(function(){
+    console.log("save clicked");
+
+    textArea = $($(this).parent().parent().children()[1]);
+
+    input = textArea.val();
+    iD = textArea.attr("id");
+
+    alterStoredInput(input, iD);
+    getStoredInput();
+});
+
